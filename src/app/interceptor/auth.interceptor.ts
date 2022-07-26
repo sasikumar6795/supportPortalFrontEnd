0d47946f4b3a2323b78d6428881c828f4937d00e
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-07-26 21:36:28 +0530

    interceptor completed for the httprequest

---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../service/authentication.service';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationService:AuthenticationService) {}
+
+  intercept(httpRequest: HttpRequest<unknown>, httpHandler: HttpHandler): Observable<HttpEvent<unknown>> {
+    if(httpRequest.url.includes(`${this.authenticationService.host}/user/login`))
+    {
+      return httpHandler.handle(httpRequest);
+    }
+    if(httpRequest.url.includes(`${this.authenticationService.host}/user/register`))
+    {
+      return httpHandler.handle(httpRequest);
+    }
+    if(httpRequest.url.includes(`${this.authenticationService.host}/user/resetPassword`))
+    {
+      return httpHandler.handle(httpRequest);
+    }
+
+    this.authenticationService.loadToken();
+    const token = this.authenticationService.getToken();
+    //you cannot modify the original request you can just clone it
+    const interceptedRequested = httpRequest.clone({ setHeaders : {Authorization: `Bearer ${token}`} });
+    return httpHandler.handle(interceptedRequested);
+  }
+}
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -9,7 +9,7 @@ import { JwtHelperService } from "@auth0/angular-jwt";
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private host = environment.apiUrl;
+  public host = environment.apiUrl;
   private token : string;
   private loggedInUserName : string;
   private jwtHelper =  new JwtHelperService();
