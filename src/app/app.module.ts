479f8518aea5ea53eba4104bb83be85f1c8dc870
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-08-03 20:42:56 +0530

    login completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NotificationService } from './service/notification.service';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { UserComponent } from './user/user.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NotificationModule 
+    NotificationModule,
+    FormsModule 
   ],
   providers: [AuthenticationGuard,NotificationService,AuthenticationService,UserService, {provide : HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true}],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse, HttpEventType, HttpResponse } from '@angular/common/http';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NotificationType } from '../enum/notification-type.enum';
+import { User } from '../model/User';
+import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  public showLoading: boolean;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private router: Router, 
+              private authenticationService:AuthenticationService,
+              private notifier: NotificationService) { }
+  
 
   ngOnInit(): void {
+    if(!this.authenticationService.isLoggedIn()){
+      this.router.navigateByUrl('/login');
+    } else {
+      this.router.navigateByUrl('/users/management');
+    }
+  }
+
+  public onLogin(user: User): void {
+    this.showLoading =true;
+    this.subscriptions.push(
+      this.authenticationService.login(user).subscribe(
+        (response:HttpResponse<User>) => {
+          const token = response.headers.get('');
+          this.authenticationService.saveToken(token);
+          this.authenticationService.addUserTolocalCache(response.body);
+          this.router.navigateByUrl('/users/management');
+          this.showLoading=false;
+
+        },
+        (errorResponse:HttpErrorResponse) => {
+          console.log(errorResponse);
+          this.sendNotificationError(NotificationType.ERROR, errorResponse.message);
+        }
+      )
+    )
+    
+  }
+  private sendNotificationError(type: NotificationType, message: string) {
+    if(message) {
+      this.notifier.showNotification(type,message);
+    }else {
+      this.notifier.showNotification(type, 'Error Occurred while logging in the user');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
